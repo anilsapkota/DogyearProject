f0a3b88d74a46feca1d26fd44d87795e955220a3
Anil Sapkota <muzicmaniac2015@gmail.com>
2022-02-21 16:50:51 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+//let us declare a variable myAge and assign it to a value 
+const myAge = 32;
+
+//assing the value 2 to the earlyYears
+let earlyYears = 2;
+
+earlyYears *=10.5;
+
+//subtract 2 from myAge and save that value in laterYears
+laterYears = myAge-2;
+
+//we are multiplying later years by 4 and assigning it to laterYears
+laterYears *=4;
+
+//adding the total years and assigning to myAgeInDogYears
+let myAgeInDogYears = earlyYears + laterYears;
+
+//store my name in lower case fomat to variable myName
+let myName ='Anil'.toLowerCase();
+
+console.log(`My name is ${myName}.I am ${myAge} years old in human years which is ${myAgeInDogYears} in dog years`);
